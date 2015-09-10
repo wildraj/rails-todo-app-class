b21ecd8232e16459129e487902c1638ff1d9ed4f
Erin Beitel & wildraj <pair+erinbeitel.wildraj@devbootcamp.com>
2015-09-10 16:49:35 -0700

    Finished basic CRUD of todo list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def active
+  end
+
+  def complete
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_path
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    p "*"*50
+    p task_params
+    p "*"*50
+
+    @task = Task.new(params.require(:task).permit(:name))
+
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name,:checked)
+  end
+end
